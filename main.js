90b15f56baffac88fddaa1f8d95f708179f74982
Tushar Srivastava <150241711+tushar4803@users.noreply.github.com>
2023-11-08 23:46:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new typed (".text", {
+    strings: ["Web Developer" , "Photographer", "Digital Marketing enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
